a9e4a33ed312825ba4292609fa4983401126c029
Solikhin <solikhinkhin645@gmail.com>
2024-09-19 12:07:43 +0700

    profile

--- lib/Mahasiswa/profile.dart
+++ lib/Mahasiswa/profile.dart
@@ -1,89 +1,15 @@
+// lib/Mahasiswa/pekerjaan.dart
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(ProfileApp());
-}
-
-class ProfileApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: ProfileScreen(),
-    );
-  }
-}
-
-class ProfileScreen extends StatelessWidget {
+class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Row(
-          children: [
-            CircleAvatar(
-              radius: 20,
-              backgroundColor: Colors.white,
-              child: Icon(Icons.person, size: 30, color: Colors.blue),
-            ),
-            SizedBox(width: 10),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('M. Isroqi Gelby', style: TextStyle(fontSize: 18)),
-                Text('2241760020', style: TextStyle(fontSize: 14)),
-              ],
-            ),
-            Spacer(),
-            IconButton(
-              icon: Icon(Icons.exit_to_app, color: Colors.red),
-              onPressed: () {
-                // Logout functionality here
-              },
-            ),
-          ],
-        ),
+        title: Text('Profile'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            ListTile(
-              leading: Icon(Icons.vpn_key),
-              title: Text('Ganti Password'),
-              trailing: Icon(Icons.arrow_drop_down),
-              onTap: () {
-                // Ganti password functionality
-              },
-            ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 3,
-        type: BottomNavigationBarType.fixed,
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.list),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.notifications),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: '',
-          ),
-        ],
-        onTap: (index) {
-          // Handle bottom navigation bar tap
-        },
+      body: Center(
+        child: Text('Ini adalah halaman profile'),
       ),
     );
   }
