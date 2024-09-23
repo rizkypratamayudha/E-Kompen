e7a4271bc4e93f252fd5dc3e4ff55e622fa192c3
rizkypratamayudha <masrizki87@gmail.com>
2024-09-23 17:34:18 +0700

    Button mulai

--- lib/widget/welcomebutton.dart
+++ lib/widget/welcomebutton.dart
@@ -8,7 +8,9 @@ class Welcomebutton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 30.0 ),
+      child: ElevatedButton(
       onPressed: () {
         // Navigasi ke halaman login saat tombol ditekan
         Navigator.push(
@@ -18,7 +20,7 @@ class Welcomebutton extends StatelessWidget {
       },
       style: ElevatedButton.styleFrom(
         padding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 100.0),
-        backgroundColor: const Color.fromARGB(93, 255, 255, 255), // Warna latar belakang tombol
+        backgroundColor: const Color.fromARGB(64, 255, 255, 255), // Warna latar belakang tombol
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(10), // Bentuk tombol
         ),
@@ -31,6 +33,9 @@ class Welcomebutton extends StatelessWidget {
           color: Colors.white, // Warna teks
         ),
       ),
+    )
+    
+    
     );
   }
 }
