44ac44de9f0feb6b19c86fd23e2b054a9a6e873f
Solikhin <solikhinkhin645@gmail.com>
2024-10-04 22:52:10 +0700

    Revisi halaman dosen

--- lib/dosen.dart
+++ lib/dosen.dart
@@ -40,155 +40,165 @@ class _DosenDashboardState extends State<DosenDashboard> {
       return;
     }
   }
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    body: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // Header Section (This part will stay fixed)
+        Padding(
+          padding: const EdgeInsets.fromLTRB(10.0, 50.0, 10.0, 20.0),
+          child: Row(
+            children: [
+              const CircleAvatar(
+                radius: 30,
+                backgroundColor: Colors.grey,
+                child: Icon(Icons.person, size: 40),
+              ),
+              const SizedBox(width: 10),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Halo',
+                    style: GoogleFonts.poppins(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'Taufik Abdus S.T',
+                    style: GoogleFonts.poppins(),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        physics: BouncingScrollPhysics(),
-        padding: const EdgeInsets.fromLTRB(10.0, 50.0, 10.0, 10.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Header Section
-            Row(
+        // Content Section (Scrollable content starts here)
+        Expanded(
+          child: SingleChildScrollView(
+            physics: const BouncingScrollPhysics(),
+            padding: const EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 10.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const CircleAvatar(
-                  radius: 30,
-                  backgroundColor: Colors.grey,
-                  child: Icon(Icons.person, size: 40),
-                ),
-                const SizedBox(width: 10),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Halo',
-                      style: GoogleFonts.poppins(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
+                // Task Summary Section
+                Container(
+                  padding: const EdgeInsets.all(20.0),
+                  decoration: BoxDecoration(
+                    color: Colors.blueAccent,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Jumlah Tugas Aktif: 2',
+                        style: GoogleFonts.poppins(
+                          color: Colors.white,
+                          fontSize: 14,
+                        ),
                       ),
-                    ),
-                    Text(
-                      'Taufik Abdus S.T',
-                      style: GoogleFonts.poppins(),
-                    ),
-                  ],
+                      const Padding(
+                        padding: EdgeInsets.only(top: 5, bottom: 5),
+                        child: Divider(
+                          color: Colors.white,
+                          thickness: 2,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                        'Status: Terdapat Pengajuan tugas',
+                        style: GoogleFonts.poppins(
+                          color: Colors.white,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+
+                // Task Status Section
+                Container(
+                  padding: const EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Status:',
+                        style: GoogleFonts.poppins(fontSize: 16),
+                      ),
+                      const SizedBox(height: 10),
+                      Row(
+                        children: [
+                          _buildStatusIndicator(Colors.black, 'Menunggu'),
+                          const SizedBox(width: 25),
+                          _buildStatusIndicator(Colors.green, 'Dalam Pekerjaan'),
+                        ],
+                      ),
+                      const SizedBox(height: 20),
+                      _buildTaskCard('Pembuatan Web', Colors.black),
+                      _buildTaskCard('Memasukkan Nilai', Colors.green),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 30),
+
+                // Task List Section
+                Container(
+                  padding: const EdgeInsets.all(20.0),
+                  decoration: BoxDecoration(
+                    color: Colors.blueAccent,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      _buildTaskSummary('Tugas Aktif', '2'),
+                      const Padding(
+                        padding: EdgeInsets.only(top: 5, bottom: 5),
+                        child: Divider(
+                          color: Colors.white,
+                          thickness: 2,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      _buildTaskSummary('Tugas Selesai', '1'),
+                      const Padding(
+                        padding: EdgeInsets.only(top: 5, bottom: 5),
+                        child: Divider(
+                          color: Colors.white,
+                          thickness: 2,
+                        ),
+                      ),
+                      const SizedBox(height: 5),
+                      _buildTaskSummary('Total Tugas', '3'),
+                    ],
+                  ),
                 ),
               ],
             ),
-            const SizedBox(height: 20),
-
-            // Task Summary Section
-            Container(
-              padding: const EdgeInsets.all(20.0),
-              decoration: BoxDecoration(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Jumlah Tugas Aktif: 2',
-                    style: GoogleFonts.poppins(
-                      color: Colors.white,
-                      fontSize: 14,
-                    ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.only(top: 5, bottom: 5),
-                    child: Divider(
-                      color: Colors.white,
-                      thickness: 2,
-                    ),
-                  ),
-                  const SizedBox(height: 5),
-                  Text(
-                    'Status: Terdapat Pengajuan tugas',
-                    style: GoogleFonts.poppins(
-                      color: Colors.white,
-                      fontSize: 14,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 20),
-
-            // Task Status Section
-            Container(
-              padding: const EdgeInsets.all(10.0),
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Status:',
-                    style: GoogleFonts.poppins(fontSize: 16),
-                  ),
-                  const SizedBox(height: 10),
-                  Row(
-                    children: [
-                      _buildStatusIndicator(Colors.black, 'Menunggu'),
-                      const SizedBox(width: 25),
-                      _buildStatusIndicator(Colors.green, 'Dalam Pekerjaan'),
-                    ],
-                  ),
-                  const SizedBox(height: 20),
-                  _buildTaskCard('Pembuatan Web', Colors.black),
-                  _buildTaskCard('Memasukkan Nilai', Colors.green),
-                ],
-              ),
-            ),
-            const SizedBox(height: 30),
-
-            // Task List Section
-            Container(
-              padding: const EdgeInsets.all(20.0),
-              decoration: BoxDecoration(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  _buildTaskSummary('Tugas Aktif', '2'),
-                  const Padding(
-                    padding: EdgeInsets.only(top: 5, bottom: 5),
-                    child: Divider(
-                      color: Colors.white,
-                      thickness: 2,
-                    ),
-                  ),
-                  const SizedBox(height: 5),
-                  _buildTaskSummary('Tugas Selesai', '1'),
-                  const Padding(
-                    padding: EdgeInsets.only(top: 5, bottom: 5),
-                    child: Divider(
-                      color: Colors.white,
-                      thickness: 2,
-                    ),
-                  ),
-                  const SizedBox(height: 5),
-                  _buildTaskSummary('Total Tugas', '3'),
-                ],
-              ),
-            ),
-          ],
+          ),
         ),
-      ),
-      // Using BottomNavBar
-      bottomNavigationBar: BottomNavBarDosen(
-        selectedIndex: _selectedIndex,
-        onItemTapped: _onItemTapped,
-      ),
-      backgroundColor: Colors.white,
-    );
-  }
+      ],
+    ),
+    // Using BottomNavBar
+    bottomNavigationBar: BottomNavBarDosen(
+      selectedIndex: _selectedIndex,
+      onItemTapped: _onItemTapped,
+    ),
+    backgroundColor: Colors.white,
+  );
+}
+
 
   // Status Indicator Widget
   Widget _buildStatusIndicator(Color color, String label) {
